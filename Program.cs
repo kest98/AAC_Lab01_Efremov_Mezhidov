e5d41f47eecdfdcc966594aa0df10e6a32c2e892
kest98 <kiruhasnezha31@gmail.com>
2026-09-17 11:24:06 +0300

    Lab01: System monitor

--- Program.cs
+++ Program.cs
@@ -1,15 +1,44 @@
-﻿Console.WriteLine($"Компьютер: {Environment.MachineName}");
+﻿// Console.WriteLine($"Компьютер: {Environment.MachineName}");
+// Console.WriteLine($"Пользователь: {Environment.UserName}");
+// Console.WriteLine($"Дата и время: {DateTime.Now:dd.MM.yyyy HH:mm}");
+
+
+// Console.WriteLine($"OC: {Environment.OSVersion}");
+// Console.WriteLine($"64-битная OC: {Environment.Is64BitOperatingSystem}");
+
+
+// Console.WriteLine($"Логических процессоров: {Environment.ProcessorCount}");
+
+
+// Console.WriteLine($"PID процесса: {Environment.ProcessId}");
+// long memory = Environment.WorkingSet / 1024 / 1024;
+// Console.WriteLine($"Память процесса: {memory}МБ");
+
+
+using System.Diagnostics;
+
+Console.WriteLine("МИНИ-МОНИТОР СИСТЕМЫ\n");
+Console.WriteLine($"Компьютер: {Environment.MachineName}");
 Console.WriteLine($"Пользователь: {Environment.UserName}");
-Console.WriteLine($"Дата и время: {DateTime.Now:dd.MM.yyyy HH:mm}");
-
-
 Console.WriteLine($"OC: {Environment.OSVersion}");
 Console.WriteLine($"64-битная OC: {Environment.Is64BitOperatingSystem}");
-
-
 Console.WriteLine($"Логических процессоров: {Environment.ProcessorCount}");
 
-
+Process currentProcess = Process.GetCurrentProcess();
 Console.WriteLine($"PID процесса: {Environment.ProcessId}");
-long memory = Environment.WorkingSet / 1024 / 1024;
-Console.WriteLine($"Память процесса: {memory}МБ");
+Console.WriteLine("Память процессора:");
+
+
+for (int i = 1; i <= 3; i++)
+{
+    currentProcess.Refresh();
+    long memory = currentProcess.WorkingSet64 / 1024 / 1024;
+    Console.WriteLine($"{i} измерение: {memory} МБ");
+    if (i < 3)
+    {
+        Console.WriteLine("Обновите окно через несколько секунд...");
+        Console.ReadLine();
+    }
+}
+
+Console.WriteLine("Исследование завершено.");
